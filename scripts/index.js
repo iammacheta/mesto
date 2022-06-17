bb26282c884f2428807e432e08cc2dc1be64bb03
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2022-06-17 15:46:32 +0400

    feat: Добавить открытие изображения в фуллскрин

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 // Находим модальное окно PROFILE
 const popupElementProfile = document.querySelectorAll('.popup')[0]
 // Находим кнопку закрытия модального окна профиля
@@ -23,9 +22,10 @@ const editButton = profile.querySelector('.profile__edit-button')
 
 // Функция открытия поп-апа profile
 function openPopupProfile(evt) {
-    popupElementProfile.classList.add('popup_opend')
-    nameInput.value = profileName.textContent
-    jobInput.value = profileJob.textContent
+  popupElementProfile.classList.add('popup_opend')
+  console.log('click to add button')
+  nameInput.value = profileName.textContent
+  jobInput.value = profileJob.textContent
 }
 
 // Функция закрытия поп-апа profile
@@ -35,19 +35,19 @@ function closePupupProfile(evt) {
 
 // Обработчик «отправки» формы profile
 function formSubmitHandlerProfile(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
 
-    // Получаем значение полей jobInput и nameInput из свойства value
-    let nameInputValue = nameInput.value
-    let jobInputValue = jobInput.value
+  // Получаем значение полей jobInput и nameInput из свойства value
+  let nameInputValue = nameInput.value
+  let jobInputValue = jobInput.value
 
-    // Заменяем текст в элементах профиля на введенное содержимое полей ввода
-    profileName.textContent = nameInputValue
-    profileJob.textContent = jobInputValue
+  // Заменяем текст в элементах профиля на введенное содержимое полей ввода
+  profileName.textContent = nameInputValue
+  profileJob.textContent = jobInputValue
 
-    // При сохранении данных из формы, нужно ее закрыть
-    popupElementProfile.classList.remove('popup_opend')
+  // При сохранении данных из формы, нужно ее закрыть
+  popupElementProfile.classList.remove('popup_opend')
 }
 
 // Прикрепляем обработчик к кнопке сохранить в форме profile:
@@ -120,67 +120,106 @@ addButton.addEventListener('click', openPopupCard)
 // Обработка кнопки крестика, закрываем форму редактирования карточки
 closeButtonCard.addEventListener('click', closePupupCard)
 
+
+
+
+// Находим модальное окно FULLSCREEN
+const popupElementFullscreen = document.querySelectorAll('.popup')[2]
+// Находим кнопку закрытия модального окна card
+const closeButtonFullscreen = popupElementFullscreen.querySelector('.popup__close-button')
+
+// Обработка кнопки крестика, закрываем фуллскрин
+closeButtonFullscreen.addEventListener('click', closePupupFullscreen)
+
+// Функция открытия поп-апа фуллскрин
+function openPopupFullscreen(evt) {
+  popupElementFullscreen.classList.add('popup_opend')
+}
+
+// Функция закрытия поп-апа фуллскрин
+function closePupupFullscreen(evt) {
+  popupElementFullscreen.classList.remove('popup_opend')
+}
+
+// Находим элемент figure
+const figureElement = document.querySelector('.popup__fullscreen')
+
+// Находим элемент фулскрин картинку
+const imageFullscreen = figureElement.querySelector('.popup__image')
+
+// Находим элемент подпись к фулскрин картинке
+const imageCaption = figureElement.querySelector('.popup__caption')
+
 const initialCards = [
-    {
-      name: 'Париж',
-      link: 'https://images.unsplash.com/photo-1570168201769-8f2ca2e012de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1182&q=80'
-    },
-    {
-      name: 'Нью-Йорк',
-      link: 'https://images.unsplash.com/photo-1589251204996-3367cc27f084?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1155&q=80'
-    },
-    {
-      name: 'Тбилиси',
-      link: 'https://images.unsplash.com/photo-1589308955174-ad82f6416790?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1198&q=80'
-    },
-    {
-      name: 'Стамбул',
-      link: 'https://images.unsplash.com/photo-1650802314281-50646ff3f65c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1181&q=80'
-    },
-    {
-      name: 'Санторини',
-      link: 'https://images.unsplash.com/photo-1557701434-aa5a992f0343?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
-    },
-    {
-      name: 'Австралия',
-      link: 'https://images.unsplash.com/photo-1571690388560-4d0282e6b1d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1530&q=80'
-    }
-  ]
+  {
+    name: 'Париж',
+    link: 'https://images.unsplash.com/photo-1570168201769-8f2ca2e012de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1182&q=80'
+  },
+  {
+    name: 'Нью-Йорк',
+    link: 'https://images.unsplash.com/photo-1589251204996-3367cc27f084?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1155&q=80'
+  },
+  {
+    name: 'Тбилиси',
+    link: 'https://images.unsplash.com/photo-1589308955174-ad82f6416790?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1198&q=80'
+  },
+  {
+    name: 'Стамбул',
+    link: 'https://images.unsplash.com/photo-1650802314281-50646ff3f65c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1181&q=80'
+  },
+  {
+    name: 'Санторини',
+    link: 'https://images.unsplash.com/photo-1557701434-aa5a992f0343?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
+  },
+  {
+    name: 'Австралия',
+    link: 'https://images.unsplash.com/photo-1571690388560-4d0282e6b1d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1530&q=80'
+  }
+]
 
 //   Добавляем 6 карточек из "коробки" через JavaScript
-  const galleryContainer = document.querySelector('.gallery')
-  const cardTemplate = document.querySelector('#card-template').content
+const galleryContainer = document.querySelector('.gallery')
+const cardTemplate = document.querySelector('#card-template').content
 
 
 // Объявляем функцию для создания карточки
-  function createCard(element) {
-    const cardElement = cardTemplate.querySelector('.gallery__card').cloneNode(true)
-    const cardImage = cardElement.querySelector('.gallery__image')
-    
-    // Присваиваем значения атрибутам картинок
-    cardImage.src = element.link
-    cardImage.alt = element.name
-    
-    // Подпись карточки
-    cardElement.querySelector('.gallery__text').textContent = element.name
+function createCard(element) {
+  const cardElement = cardTemplate.querySelector('.gallery__card').cloneNode(true)
+  const cardImage = cardElement.querySelector('.gallery__image')
 
-    // Добавляем слушатель на кнопку лайк
-    const cardLike = cardElement.querySelector('.gallery__like')
-    cardLike.addEventListener('click', function(evt) {
-      evt.target.classList.toggle('gallery__like_active')
-    })
+  // Присваиваем значения атрибутам картинок
+  cardImage.src = element.link
+  cardImage.alt = element.name
 
-    // Добавляем слушатель на кнопку удалить
-    const cardDelete = cardElement.querySelector('.gallery__delete')
-    cardDelete.addEventListener('click', (evt) => {
-      evt.target.parentElement.remove()
-    })
+  // Подпись карточки
+  cardElement.querySelector('.gallery__text').textContent = element.name
 
-    // Добавляем карточку в начало галереи
-    galleryContainer.prepend(cardElement)
-  } 
-
-  // Проходимся по всем элементам и создаем карточки 
-  initialCards.forEach(element => {
-    createCard(element)
+  // Добавляем слушатель на кнопку лайк
+  const cardLike = cardElement.querySelector('.gallery__like')
+  cardLike.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('gallery__like_active')
   })
+
+  // Добавляем слушатель на кнопку удалить
+  const cardDelete = cardElement.querySelector('.gallery__delete')
+  cardDelete.addEventListener('click', (evt) => {
+    evt.target.parentElement.remove()
+  })
+
+  // Добавляем слушатель на открытие в фуллскрин
+  cardImage.addEventListener('click', (evt) => {
+    openPopupFullscreen()
+    imageFullscreen.src = element.link
+    imageFullscreen.alt = element.name
+    imageCaption.textContent = element.name
+
+  })
+
+  // Добавляем карточку в начало галереи
+  galleryContainer.prepend(cardElement)
+}
+
+// Проходимся по всем элементам и создаем карточки 
+initialCards.forEach(element => {
+  createCard(element)
+})
