6b8d4b0572dfa42e0f28a561396c0c3bed5ccbe9
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2022-06-20 18:07:39 +0400

    fix: вынести данные карточке в отдельный js файл

---
+++ scripts/initial-cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Париж',
+      link: 'https://images.unsplash.com/photo-1570168201769-8f2ca2e012de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1182&q=80'
+    },
+    {
+      name: 'Нью-Йорк',
+      link: 'https://images.unsplash.com/photo-1589251204996-3367cc27f084?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1155&q=80'
+    },
+    {
+      name: 'Тбилиси',
+      link: 'https://images.unsplash.com/photo-1589308955174-ad82f6416790?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1198&q=80'
+    },
+    {
+      name: 'Стамбул',
+      link: 'https://images.unsplash.com/photo-1650802314281-50646ff3f65c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1181&q=80'
+    },
+    {
+      name: 'Санторини',
+      link: 'https://images.unsplash.com/photo-1557701434-aa5a992f0343?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
+    },
+    {
+      name: 'Австралия',
+      link: 'https://images.unsplash.com/photo-1571690388560-4d0282e6b1d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1530&q=80'
+    }
+  ]
